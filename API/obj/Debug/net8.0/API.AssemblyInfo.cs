540ab64a65a7b76cad83c36effefb514ce25fb8d
Nimira43 <n.rabone72@aol.com>
2024-08-31 14:36:16 +0100

    Using the sqlite explorer for the first time to create some mock data for the newly created dating.db

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717659563098bf34915ef5813615aff17ad919cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b456b4c80b9828a5f4836c5c43c3415cbf3136")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
