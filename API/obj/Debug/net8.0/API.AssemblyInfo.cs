82e6ca337995375835812bcef39705c2c5277b17
Nimira43 <n.rabone72@aol.com>
2024-08-31 16:11:42 +0100

    Start to turn requests to asynchronous

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28b8daa0b80406e3c942d4c8457735b88d12b6fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f5ab1f95857ebaec5ac1e3745adb1c35f2d877")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
