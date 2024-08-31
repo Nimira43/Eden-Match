752030f42ecd23c5957f3db2c3d86e1b532e87c7
Nimira43 <n.rabone72@aol.com>
2024-08-31 10:09:29 +0100

    Updating boilerplate

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a940dc1715013ed8fea18e2f799a7f43f118b52c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c8ecb9c3aef6e70672bfe9e1dbbe146b460bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
