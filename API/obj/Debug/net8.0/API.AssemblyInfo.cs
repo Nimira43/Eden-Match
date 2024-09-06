31d75786533bc2704ec5565d39b353d352120931
Nimira43 <n.rabone72@aol.com>
2024-09-07 00:06:44 +0100

    Solving Cors issues between the frontend and backend.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d66cce2ccbca0a63fc03e9632f3a3cd77b100813")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f9086c1b5877d3b9bd9023527e64889a8f2760")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
