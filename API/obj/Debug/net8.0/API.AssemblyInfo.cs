b9e84b8059541be01a6acfb8c3fe8a4da1f2df9a
Nimira43 <n.rabone72@aol.com>
2024-09-18 04:43:39 +0100

    Brought in Bootstrap and Fontawesome and ngx-bootstrap

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fca20a9980801006a90b1c509f21aa83df8b175")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ac2d42d481db24cfe183d3ebf2850bd3fcbe72")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
