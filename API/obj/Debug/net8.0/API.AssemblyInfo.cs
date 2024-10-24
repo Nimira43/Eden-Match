5b80a939f477650aba12f8543e9f382a2cf5ffc8
Nimira43 <n.rabone72@aol.com>
2024-10-24 04:22:29 +0100

    Created Entities model folder and starting to create AppUser.cs

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e0328825a8923752c883b61ad2d6dda5c36018")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90b9f5b036032b7d1492b957980f99a424a46b50")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
