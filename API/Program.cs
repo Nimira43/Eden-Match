95fab00d856ec8252e7eebbf17fca378b3beda35
Nimira43 <n.rabone72@aol.com>
2025-07-19 16:01:28 +0100

    Register AppDbContext

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
+using API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>
 
 var app = builder.Build();
 
