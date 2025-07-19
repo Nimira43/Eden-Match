bb44689323abc1a6befb435252e1b2335431b65f
Nimira43 <n.rabone72@aol.com>
2025-07-19 15:23:22 +0100

    Cleaned up boilerplate

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Piquet", "Prost", "Cool", "Mild", "Warm", "Mansell", "Senna", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
