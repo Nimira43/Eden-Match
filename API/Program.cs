3077dc9fc8a319e353e58dd723c2f9b1fd1d52d4
Nimira43 <n.rabone72@aol.com>
2025-07-19 16:03:32 +0100

    Options - UseSqlite

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,13 @@
 using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
