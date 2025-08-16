68f591cafaa345c8a9755ab3065129ce5efa88f7
Nimira43 <n.rabone72@aol.com>
2025-08-16 09:35:12 +0100

    Register - 1

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -8,5 +9,8 @@ namespace API.Controllers;
 public class AccountController(AppDbContext context) : BaseApiController
 {
   [HttpPost("register")]
-  public async Task<ActionResult<AppUser>>
+  public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+  {
+    
+  } 
 }
