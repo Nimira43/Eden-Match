e5c585b58240ffd2c25c81ba12f1a2bb260879c7
Nimira43 <n.rabone72@aol.com>
2025-08-16 09:11:27 +0100

    Returning AppUser

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,10 +1,12 @@
 using System;
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-public class AccountController(AppDbContext context): BaseApiController
+public class AccountController(AppDbContext context) : BaseApiController
 {
   [HttpPost("register")]
+  public async Task<ActionResult<AppUser>>
 }
