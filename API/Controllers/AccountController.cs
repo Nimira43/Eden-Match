49dd65f787c59d5a62f150c3561dfbf043bec099
Nimira43 <n.rabone72@aol.com>
2025-08-16 09:08:58 +0100

    Creating Post request

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,9 +1,10 @@
 using System;
 using API.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 public class AccountController(AppDbContext context): BaseApiController
 {
-
+  [HttpPost("register")]
 }
