6f500511ba375d010e617f0cb4dc596f10c6c290
Nimira43 <n.rabone72@aol.com>
2025-08-25 20:15:45 +0100

    Starting to define RegisterDto class

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,5 +4,7 @@ namespace API.DTOs;
 
 public class ResgisterDto
 {
-
+  public required string DisplayName { get; set; }
+  public required string Email { get; set; }
+  public required string Password { get; set; }
 }
