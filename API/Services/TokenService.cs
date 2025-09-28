6ae1c84222b504c8e3c37db8de8f181401e29a57
Nimira43 <n.rabone72@aol.com>
2025-09-28 05:04:01 +0100

    CreateToken - 3

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -8,5 +8,6 @@ public class TokenService(IConfiguration config) : ITokenService
   public string CreateToken(AppUser user)
   {
     var tokenKey = config["TokenKey"];
+    if (tokenKey.Length < 64) throw new Exception("Your token key needs to be  >= 64 characters.");
   }
 }
