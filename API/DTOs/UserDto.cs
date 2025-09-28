4777b2dc1534f204d0205828d65e85533d6f3a6d
Nimira43 <n.rabone72@aol.com>
2025-09-28 05:37:30 +0100

    UserDto - 2

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -8,4 +8,5 @@ public class UserDto
   public required string Email { get; set; }
   public required string DisplayName { get; set; }
   public string? ImageUrl { get; set; }
+  public required string Token { get; set; }
 }
