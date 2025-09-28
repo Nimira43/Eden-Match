a2ba17f9d87694cbeac6586d77df7271af32d2ed
Nimira43 <n.rabone72@aol.com>
2025-09-28 05:13:54 +0100

    CreateToken - 5

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,5 +1,7 @@
+using System.Text;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services;
 
@@ -7,8 +9,9 @@ public class TokenService(IConfiguration config) : ITokenService
 {
   public string CreateToken(AppUser user)
   {
-    var tokenKey = config["TokenKey" ] ?? throw new Exception("Cannot get token key");
+    var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot get token key");
     if (tokenKey.Length < 64)
-      throw new Exception("Your token key needs to be  >= 64 characters.");
+      throw new Exception("Your token key needs to be >= 64 characters.");
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
   }
 }
