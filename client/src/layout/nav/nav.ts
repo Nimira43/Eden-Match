a2e39a335adb392502df2ecf7feb5d27b4c150b9
Nimira43 <n.rabone72@aol.com>
2025-10-13 03:46:12 +0100

    Updating Nav - 1

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core' 
+import { Component, inject } from '@angular/core' 
 import { FormsModule } from '@angular/forms'
+import { AccountService } from '../../core/services/account-service'
 
 @Component({
   selector: 'app-nav',
@@ -8,6 +9,7 @@ import { FormsModule } from '@angular/forms'
   styleUrl: './nav.css'
 })
 export class Nav {
+  private account = inject(AccountService)
   protected creds: any = {}
 
   login() {
