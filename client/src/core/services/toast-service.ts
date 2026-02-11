fddd81f8359d76a534ada229ad243e478d63aa75
Nimira43 <n.rabone72@aol.com>
2026-02-11 19:53:07 +0000

    Creating ToastService class.

---
+++ client/src/core/services/toast-service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+  constructor() { 
+    this.createToastContainer()
+  }
+  
+  private createToastContainer() {
+    if (!document.getElementById('toast-container')) {
+      const container = document.createElement('div')
+      container.id = 'toast-container'
+      container.id = 'toast toast-bottom toast-end'
+      document.body.appendChild(container)
+    }
+  }
+
+  private createToastElement(
+    message: string,
+    alertClass: string,
+    duration = 5000
+  ) {
+    const toastContainer = document.getElementById('toast-container')
+    
+    if (!toastContainer) return
+
+    const toast = document.createElement('div')
+    toast.classList.add('alert', alertClass, 'shadow-lg')
+    toast.innerHTML = `
+      <span>${message}</span>
+      <button class='ml-4 btn dark-btn'>x</button>
+    `
+
+    toast.querySelector('button')?.addEventListener('click', () => {
+      toastContainer.removeChild(toast)
+    })
+
+    toastContainer.append(toast)
+
+    setTimeout(() => {
+      if (toastContainer.contains(toast)) {
+        toastContainer.removeChild(toast)
+      }
+    }, duration)
+  }
+}
