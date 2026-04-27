92e901f61d6c341decb2e955c5cef60a0ac1361c
Nimira43 <n.rabone72@aol.com>
2026-04-27 01:21:48 +0100

    Work on displaying member list and member page.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { InitService } from '../core/services/init-service'
 import { lastValueFrom } from 'rxjs'
 import { errorInterceptor } from '../core/interceptors/error-interceptor'
+import { jwtInterceptor } from '../core/interceptors/jwt-interceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,7 +16,7 @@ export const appConfig: ApplicationConfig = {
       routes, withViewTransitions()
     ),
     provideHttpClient(
-      withInterceptors([errorInterceptor])
+      withInterceptors([errorInterceptor, jwtInterceptor])
     ),
     provideAppInitializer(async () => {
       const initService = inject(InitService)
