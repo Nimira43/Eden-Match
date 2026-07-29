d9639a33bfc3b2178124c5a5ee15c10dbc412642
Nimira43 <n.rabone72@aol.com>
2026-07-29 17:28:14 +0100

    StarButton component.

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -10,7 +10,7 @@ import { tap } from 'rxjs'
 export class MemberService {
   private http = inject(HttpClient)
   private baseUrl = environment.apiUrl
-  editMode = signal(true)
+  editMode = signal(false)
   member = signal<Member | null>(null)
   
   getMembers() {
