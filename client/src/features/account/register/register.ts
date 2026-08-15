436cafcd9f3914d60f975dd218ca64286b598430
Nimira43 <n.rabone72@aol.com>
2026-08-15 04:05:12 +0100

    Password validation.

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit, output } from '@angular/core'
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RegisterCreds } from '../../../types/user'
 import { AccountService } from '../../../core/services/account-service';
 import { JsonPipe } from '@angular/common';
@@ -22,32 +22,27 @@ export class Register implements OnInit {
 
   initialiseForm() {
     this.registerForm = new FormGroup({
-      email: new FormControl(
-        'member@edenmatch.com',
-        [
-          Validators.required,
-          Validators.email
-        ]
-      ),
-      displayName: new FormControl(
-        '',
-        Validators.required
-      ),
-      password: new FormControl(
-        '',
-        [
-          Validators.required,
-          Validators.minLength(4),
-          Validators.maxLength(8)
-        ]
-      ),
-      confirmPassword: new FormControl(
-        '',
-        Validators.required
-      )
+      email: new FormControl('member@edenmatch.com', [Validators.required, Validators.email]),
+      displayName: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    })
+    this.registerForm.controls['password'].valueChanges.subscribe(() => {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity()
     })
   }
   
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent
+
+      if (!parent) return null
+
+      const matchValue = parent.get(matchTo)?.value
+      return control.value === matchValue ? null : {passwordMismatch: true}
+    }
+  }
+
   register() {
     console.log(this.registerForm.value)
     // this.accountService.register(this.creds).subscribe({
