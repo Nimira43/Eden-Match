157230d8adfd2aa08f6afa5272b2d3986e28603b
Nimira43 <n.rabone72@aol.com>
2026-08-24 18:26:50 +0100

    Completing Register form with input validations.

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -3,10 +3,11 @@ import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validatio
 import { RegisterCreds } from '../../../types/user'
 import { AccountService } from '../../../core/services/account-service';
 import { JsonPipe } from '@angular/common';
+import { TextInput } from "../../../shared/text-input/text-input";
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
@@ -22,7 +23,7 @@ export class Register implements OnInit {
 
   initialiseForm() {
     this.registerForm = new FormGroup({
-      email: new FormControl('member@edenmatch.com', [Validators.required, Validators.email]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       displayName: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
       confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
