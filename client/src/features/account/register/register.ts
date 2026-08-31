d4b7cf1b6872500736718adcc23dae58277ca973
Nimira43 <n.rabone72@aol.com>
2026-08-31 10:11:36 +0100

    Profile form.

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -2,12 +2,11 @@ import { Component, inject, output, signal } from '@angular/core'
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RegisterCreds } from '../../../types/user'
 import { AccountService } from '../../../core/services/account-service';
-import { JsonPipe } from '@angular/common';
 import { TextInput } from "../../../shared/text-input/text-input";
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, JsonPipe, TextInput],
+  imports: [ReactiveFormsModule, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
@@ -58,6 +57,12 @@ export class Register {
 
   prevStep() {
     this.currentStep.update(prevStep => prevStep - 1)
+  }
+
+  getMaxDate() {
+    const today = new Date()
+    today.setFullYear(today.getFullYear() - 18)
+    return today.toISOString().split('T')[0]
   }
 
   register() {
